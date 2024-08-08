372e9b498f20681f80411fcb5f7978c57cd05542
ghenriqu3 <ghsousa.silva@gmail.com>
2024-08-07 21:14:49 -0300

    add route getOne and deleteOne

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,15 +5,31 @@ const getAllproducts = async (req, res) => {
         const allProducts = await products.findAll();
         res.json(allProducts);
 }
-const createProducts = async (req, res) => {
-    const {producName, category, price} = await req.body
-    const createProd = await products.create({producName, category, price});
 
-    // console.log(createProd)
-    res.json(createProd);
+const getOne = async (req, res) => {
+    const productId =  parseInt(req.params.id)
+    const product = await products.findByPk(productId)
+    res.json(product)
 }
+
+const createProducts = async (req, res) => {
+    const {productName, category, price} = await req.body
+    const createProd = await products.create({productName, category, price});
+    res.json(createProd); 
+}
+
+const deleteOne = async (req, res) => {
+    const deleteProdId = parseInt(req.params.id)
+    const deleteProduct = await products.destroy({
+        where: {id : deleteProdId}
+    })
+    res.json({message: "usuario deletado com sucesso"})
+}
+
 
 module.exports = { 
     getAllproducts,
-    createProducts
+    createProducts,
+    getOne,
+    deleteOne
 }
--- routes/products.js
+++ routes/products.js
@@ -4,5 +4,7 @@ const productsController = require('../controllers/productsController')
 
 /* GET users listing. */
 router.get('/', productsController.getAllproducts)
+router.get('/:id', productsController.getOne)
+router.delete('/:id', productsController.deleteOne)
 router.post('/', productsController.createProducts)
 module.exports = router;
